0cd1b5839129f17957e414dd1fead75629a6a7b0
Elliot Watson <a0916228544@gmail.com>
2021-06-17 15:06:09 +0800

    modify download_data.sh too

--- sh/download_data.sh
+++ sh/download_data.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 kaggle datasets download -d paultimothymooney/chest-xray-pneumonia
-unzip chest-xray-pneumonia.zip -d ..
-mv ../chest_xray ../datasets
+unzip chest-xray-pneumonia.zip
+mv chest_xray datasets
 rm chest-xray-pneumonia.zip
